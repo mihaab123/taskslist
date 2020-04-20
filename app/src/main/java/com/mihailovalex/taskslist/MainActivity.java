b6ad8a7fc8a7870c080a37f40c081e13ced59c22
Alexander Mihailov <miha.ab.2017@gmail.com>
2020-04-20 15:14:21 +0300

    begin toolbar

--- app/src/main/java/com/mihailovalex/taskslist/MainActivity.java
+++ app/src/main/java/com/mihailovalex/taskslist/MainActivity.java
@@ -1,14 +1,53 @@
 package com.mihailovalex.taskslist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Spinner spinner;
+    String[] data = {"one", "two", "three", "four", "five"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToobar();
+        initSpinner();
+    }
+
+    private void initSpinner() {
+        // адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, data);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner = findViewById(R.id.spinner_groups);
+        spinner.setAdapter(adapter);
+        // выделяем элемент
+        spinner.setSelection(1);
+    }
+
+    private void initToobar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /*if(item.getItemId() == R.id.action_search) {
+            Intent intent = new Intent(this, SearchUsersActivity.class);
+            startActivity(intent);
+        }*/
+        return true;
     }
 }
